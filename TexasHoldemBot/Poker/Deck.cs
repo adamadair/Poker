410b32654911db5c517f45202b610399db7acf74
Adam Adair <adamadair@outlook.com>
2018-10-05 09:34:15 -0500

    deck comments

--- TexasHoldemBot/Poker/Deck.cs
+++ TexasHoldemBot/Poker/Deck.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TexasHoldemBot.Poker
 {
+    /// <summary>
+    /// Convenience methods for dealing with an entire deck of cards.
+    /// </summary>
     public static class Deck
     {
+        /// <summary>
+        /// An array of cards that makes up an entire playing deck for
+        /// Texas Hold'em. 
+        /// </summary>
         private static readonly Card[] _deck = Card.Cards.Values.ToArray();
 
+        /// <summary>
+        /// Get the full deck
+        /// </summary>
+        /// <returns></returns>
         public static List<Card> FullDeck()
         {
             return new List<Card>(_deck);
         }
 
+        /// <summary>
+        /// Get a deck, minus some cards.
+        /// </summary>
+        /// <param name="outs">list of cards NOT in the deck</param>
+        /// <returns>The sub deck</returns>
         public static List<Card> GetSubDeck(IEnumerable<Card> outs)
         {
             var l = new List<Card>(_deck);
@@ -25,6 +39,11 @@ namespace TexasHoldemBot.Poker
             return l;
         }
 
+        /// <summary>
+        /// Get all two card combinations from a collection of cards
+        /// </summary>
+        /// <param name="cards"></param>
+        /// <returns></returns>
         public static IEnumerable<Tuple<Card, Card>> GetTwoCardCombinations(IEnumerable<Card> cards)
         {
             var l = new List<Tuple<Card, Card>>();
